029faa999fa0ffd6208d41a83a8b547abad98544
Austin McCarthy <me@austinmccarthy.com>
2014-12-30 09:46:21 -0500

    Dec 30

---
+++ Lions and Tigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  Lions and Tigers
+//
+//  Created by Austin McCarthy on 12/18/14.
+//  Copyright (c) 2014 austinmccarthy.com. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
